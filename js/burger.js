771181848af3279174cc0926d9113f0a762dda6c
SL <serega.lypnik@gmail.com>
2018-12-10 07:11:40 +0200

    Before exit

---
+++ js/burger.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#burg").click(function(){
+        $("#mobmenu").show(300);
+    });
+    $("#close").click(function(){
+        $("#mobmenu").hide(300);
+    });
+});
+//подключить скрипт
+//добавить хтмл меню
+//изменить основное меню
